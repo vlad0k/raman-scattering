b49aefd9f3b0be330e111230db154f12c11f8268
vlad0k <vladok.sha089@gmail.com>
2021-05-01 18:53:05 +0300

    feat: charnt fiel energy change

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from math import sqrt, exp, cos
+from math import sqrt, exp, cos, pi
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+from numba import jit, cuda
 
 
 i = 1j
@@ -16,7 +17,6 @@ wave_length = 2.4 * 10**-12
 e = 4.8 * 10**-10 
 c = 2.9979 * 10**10
 m = 9.1094 * 10**-28
-pi = 3.14
 
 beta = 1 # берем 1
 
@@ -25,12 +25,13 @@ gamma0 = 4
 uii = c * sqrt(1 - (1 / gamma0)**2)
 w = 1 * 10**12 # -s # частота волны
 Omega = 10 ** 11 # -s
-T = 6 * 10**-12
-t0 = 0
-t1 = T
-n = 2000
+T = 2 * pi / w
 
-kii = (w - Omega) / uii
+t0 = 0
+t1 = 10*T
+n = 1000
+
+kii = (w + Omega) / uii
 
 lmbda = 2 * pi / kii
 
@@ -42,7 +43,6 @@ def z_n(z0, t): # z невозмущенное
   return z0 + uii * (t - t0)
   
 def E(z,t): # wave field
-  # return 0.5 * abs(Eii) * сexp(-i * w * t + i * kii * z) * 2
   return 0.5 * abs(Eii) * cos(-w * t + kii * z) * 2
 
 def W (u): # relativistic kinetic energy u - current speed 
@@ -55,9 +55,8 @@ def system_solve(z0, t_list):
     return [dzdt, dvdt]
 
   s0=[z0, uii]
-  s = odeint(F, s0, t_list)
+  s = odeint(F, s0, t_list, tcrit=4)
   return s
-
 
 def find_electron_energy(z0, T):  #z0 - зависит от положения на волне
   U = [u[1] for u in system_solve(z0, T)]
@@ -71,9 +70,21 @@ T_list = np.linspace(t0,t1, n, endpoint=False) # 1s interval for 30s
 
 electron_energies = [find_electron_energy(z0, T_list) for z0 in Z]
 
-W_mean = [np.mean(i) for i in np.array(electron_energies).transpose()]
+W_mean = [np.mean(i) for i in np.array(electron_energies).transpose()] #энергия пучка
 
-plt.plot(T_list,W_mean)
+E0= [] # начальная энергия для каждого электрона для каждого момента времени
+for t in T_list:
+  index = len(E0)
+  E0.append([])
+  for z in Z:
+    E0[index].append(E(z, t))
+
+E = [np.mean(i) for i in np.array(E0).transpose()] # начальная энергия поля в каждый момент времени
+
+
+y = [-(E[i] - W_mean[i]) for i in range(len(W_mean))] # изменение энергии поля, где (0.5 * e * E[i]) - начальная энергия, 
+
+plt.plot(T_list,y)
 plt.show()
 
 
